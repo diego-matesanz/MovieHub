471d673a8ebe60ab985a72ef791707f2dbdc6b47
Diego Matesanz <diegomatesanzfuente@gmail.com>
2023-10-04 20:19:00 +0200

    Ofuscated api key

--- app/src/main/java/com/diego/matesanz/moviehub/MoviesService.kt
+++ app/src/main/java/com/diego/matesanz/moviehub/MoviesService.kt
@@ -4,6 +4,6 @@ import retrofit2.http.GET
 
 interface MoviesService {
 
-    @GET("discover/movie?api_key=6001b103e9cf4ad105cb25a8571de820")
+    @GET("discover/movie?api_key=${ApiKey.apiKey}")
     suspend fun getMovies(): MoviesResult
 }
